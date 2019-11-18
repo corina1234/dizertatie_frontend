283dd6b838e3f1076530940d931a35e2e73d0e2e
Corina Strachinariu <corina-andreea.strachinariu@sap.com>
2019-11-18 13:58:11 +0200

    initial app

---
+++ src/app/plan-cladire/plan-cladire.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    templateUrl: './plan-cladire.component.html'
+})
+
+export class PlanCladireComponent implements OnInit{
+    ngOnInit(): void {
+    }
+}
